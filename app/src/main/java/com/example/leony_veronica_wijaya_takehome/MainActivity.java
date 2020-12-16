9682b387abc9425fce8d23266506a44eff24793c
veronicalw <43403489+veronicalw@users.noreply.github.com>
2020-12-16 08:02:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/leony_veronica_wijaya_takehome/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.leony_veronica_wijaya_takehome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
